a4f99e7b83b7a1d8807b666f6e4d227c8df5d6b1
Ana Luiza Soares <50274590+soaresana@users.noreply.github.com>
2021-04-04 18:35:03 -0300

    Alterações controller

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from View.view import NasaView
+from Controller.controller import NasaController
+from Model.model import NasaModel
 
 print("="*100)
 print("NASA TECHPORT")
@@ -6,6 +8,15 @@ print("="*100)
 
 info_techport = NasaView()
 
-info_techport.input_ano(input("Digite o ano que você gostaria de pesquisar os projetos da NASA: "))
-info_techport.input_tipo(input("Digite 'json' ou 'csv' para exportar seu arquivo: "))
-print(info_techport.resposta())
+info_controller = NasaController()
+
+model = NasaModel()
+
+#info_techport.input_ano(input("Digite o ano que você gostaria de pesquisar os projetos da NASA: "))
+# info_techport.input_tipo(input("Digite 'json' ou 'csv' para exportar seu arquivo: "))
+#info_techport.input_quantidade(int(input("Digite a quantidade de imagens que você gostaria de ver: ")))
+
+#print(info_techport.imprime_infos())
+
+info_controller.infos(5)
+
